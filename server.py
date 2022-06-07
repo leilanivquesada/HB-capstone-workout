19c7c094622799b97e460305cc40460fd04d5107
Leilani Quesada <leilanivquesada@gmail.com>
2022-06-07 01:15:20 -0700

    more formatting and styling

--- server.py
+++ server.py
@@ -316,4 +316,4 @@ def logout():
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
     connect_to_db(app)
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=False)
